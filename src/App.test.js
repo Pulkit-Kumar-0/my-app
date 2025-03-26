fd0c4e39e05859372e8110153b97e87e41aa6e4b
Pulkit Kumar <pulkitkumar2711@gmail.com>
2025-03-26 13:58:53 +0530

    Removed default test file

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders MyApp header", () => {
-  render(<App />);
-  const headerElement = screen.getByText(/MyApp/i);
-  expect(headerElement).toBeInTheDocument();
-});
